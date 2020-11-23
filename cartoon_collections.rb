2b1d2b12ad57479ba2cec5cada34c713f8d53e8d
Brett Needham <brettneedham88@gmail.com>
2020-11-23 18:35:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
   #%w[ant bear cat].any? { |word| word.length >= 3 }
- planeteer_calls.any? { |call| call.length <= 4 }
+ planeteer_calls.any? { |call| call.length > 4 }
 end
 
 def find_valid_calls(planeteer_calls)
